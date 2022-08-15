5e95fc3ca3f9da953341ba9d5f8e0f0fde315a1b
JackScholten <jack.scholten@hotmail.com>
2022-08-16 00:04:11 +0930

    Prac3

--- main.cpp
+++ main.cpp
@@ -18,9 +18,17 @@ int main() {
 
 Tournament Jew;
 
-Jew.FillArray( Jew.getCompetitors() , new Toolbox() );
+
 Jew.FillArray( Jew.getCompetitors() , new Avalanche() );
 Jew.FillArray( Jew.getCompetitors() , new Bureaucrat() );
+Jew.FillArray( Jew.getCompetitors() , new Bureaucrat() );
+Jew.FillArray( Jew.getCompetitors() , new Toolbox() );
+Jew.FillArray( Jew.getCompetitors() , new Toolbox() );
+Jew.FillArray( Jew.getCompetitors() , new Crescendo() );
+Jew.FillArray( Jew.getCompetitors() , new Crescendo() );
+Jew.FillArray( Jew.getCompetitors() , new FistfullODollars() );
+
+cout << Jew.run(Jew.getCompetitors()) << endl;
 
 return 0;
 };
