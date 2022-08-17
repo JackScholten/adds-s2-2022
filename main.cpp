f4fce8f06392678c988ced11780643e2ff8c7320
JackScholten <jack.scholten@hotmail.com>
2022-08-17 11:49:17 +0930

    PracExam1

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ Document * p1;
 Book * p2 = new Book HarryPotter(123);
 
 //add book to library
-Library->addDocument(shelf, Book * p2);
+Library->addDocument(Uni->shelf, Book * p2);
 
 //checks if book is in library 
 
