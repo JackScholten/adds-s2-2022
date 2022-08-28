e65c615cb5d3c3e5d67855fcd947fd6a75ecca2f
JackScholten <Jack.scholten@hotmail.com>
2022-08-28 20:29:03 +0930

    Prac4

--- main.cpp
+++ main.cpp
@@ -10,17 +10,20 @@ int main() {
 
 Reverse a;
 
-cout << a.reverseDigit(123) << endl;
+cout << a.reverseString("reggin") << endl;
+cout << a.reverseString("reggin") << endl;
+cout << a.reverseString("reggin") << endl;
+cout << a.reverseString("reggin") << endl;
+cout << a.reverseString("reggin") << endl;
+cout << a.reverseString("reggin") << endl;
+cout << a.reverseString("reggin") << endl;
+cout << a.reverseString("reggin") << endl;
+cout << a.reverseString("reggin") << endl;
+cout << a.reverseString("reggin") << endl;
+cout << a.reverseString("reggin") << endl;
+cout << a.reverseString("reggin") << endl;
+cout << a.reverseString("reggin") << endl;
 
-cout << a.reverseString("a") << endl;
-
-Truckloads b;
-
-cout << b.numTrucks(10 , 2) << endl;
-
-EfficentTruckloads c;
-
-cout << c.numTrucks(10,2) << endl;
 
 }
 
