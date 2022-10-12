a84ca889a9f4cb309922c989f9cc5a8cc0b38742
JackScholten <jack.scholten@hotmail.com>
2022-10-12 16:08:11 +1030

    Prac8

---
+++ LinkList.h
@@ -0,0 +1,28 @@
+#ifndef _LinkList
+#define _LinkList
+
+#include "Node.h"
+
+class LinkList {
+    private:
+        Node* head;
+
+    public:
+        LinkList();
+        //LinkList(array, sizeofarray);
+        
+        void addFront(int newItem);
+        void addEnd(int newItem);
+        void addAtPosition(int position, int newItem);
+        int search(int item);
+        void deleteFront();
+        void deleteEnd();
+        void deletePosition(int position);
+        int getItem(int position);
+        void printItems();
+
+
+
+};
+
+#endif
