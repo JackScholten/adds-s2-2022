5e5bffd496fa3f42675b056db4935069294af288
JackScholten <jack.scholten@hotmail.com>
2022-10-19 10:49:51 +1030

    Prac9

---
+++ Convert.h
@@ -0,0 +1,14 @@
+#ifndef _Convert
+#define _Convert
+
+class Convert {
+    public:
+        Convert();
+
+        
+
+
+};
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <string>
+
+#include "Convert.h"
+
+using namespace std;
+
+int main() {
+
+stack<string> operators;
+queue<int> operants;
+
+
+
+return 0;
+}
